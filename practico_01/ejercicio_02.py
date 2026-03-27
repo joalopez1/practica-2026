9150df841881113dbffd329520bbbdf38af8bfd1
joalopez1 <204105785+joalopez1@users.noreply.github.com>
2026-03-27 12:13:33 -0300

    ej 02 prac 01 hecho

--- practico_01/ejercicio_02.py
+++ practico_01/ejercicio_02.py
@@ -7,7 +7,12 @@ def maximo_encadenado(a: float, b: float, c: float) -> float:
     Restricción: Utilizar UNICAMENTE tres IFs y comparaciones encadenadas.
     Referencia: https://docs.python.org/3/reference/expressions.html#comparisons
     """
-    pass # Completar
+    if a > b and a > c:
+        return a
+    if b > a and b > c:
+        return b    
+    if c > a and c > b:
+        return c
 
 
 # NO MODIFICAR - INICIO
@@ -24,7 +29,8 @@ def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
     """Re-escribir para que tome 4 parámetros, utilizar la función max.
 
     Referencia: https://docs.python.org/3/library/functions.html#max"""
-    pass # Completar
+
+    return max(a, b, c, d)
 
 
 # NO MODIFICAR - INICIO
@@ -42,7 +48,7 @@ def maximo_arbitrario(*args) -> float:
     """Re-escribir para que tome una cantidad arbitraria de parámetros.
     Referencia: https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists
     """
-    pass # Completar
+    return max(args)
 
 
 # NO MODIFICAR - INICIO
@@ -58,8 +64,9 @@ assert maximo_arbitrario(24, 9, 18, 30) == 30
 
 def maximo_recursivo(*args) -> float:
     """Re-Escribir de forma recursiva."""
-    pass # Completar
-
+    
+    return max(args[0], maximo_recursivo(*args[1:]))  #el '*' desagrupa la tupla, osea, crea elementos individuales
+    
 
 # NO MODIFICAR - INICIO
 assert maximo_recursivo(1, 10, 5, -5) == 10
